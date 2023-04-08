6f367101f9bf77077128c995e9520b32b46e8d62
ZethMaybuena <129552612+ZethMaybuena@users.noreply.github.com>
2023-04-08 17:50:38 +0400

    Update FilesInOut.java

--- FilesInOut.java
+++ FilesInOut.java
@@ -12,23 +12,14 @@ import java.lang.Number;
  */
 public class FilesInOut {
 
-    public static void main(String[] args) {
-        // Replace this with statements to set the file name (input) and file name (output).
-        // Initially it will be easier to hardcode suitable file names.
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // Enter Input file
+        System.out.println("Enter input file name:");
+        String inputFileName = in.nextLine();
+        // Enter Output file
+        System.out.println("Enter output file name:");
+        String outputFileName = in.nextLine();
 
-        // Set up a new Scanner to read the input file.
-        // Processing line by line would be sensible here.
-        // Initially, echo the text to System.out to check you are reading correctly.
-        // Then add code to modify the text to the output format.
-
-        // Set up a new PrintWriter to write the output file.
-        // Add suitable code into the above processing (because you need to do this line by line also.
-        // That is, read a line, write a line, loop.
-
-        // Finally, add code to read the filenames as arguments from the command line.
-
-        System.out.println("You need to add your own code to do anything");
-
-    } // main
-
+    }
 } // FilesInOut
